757d6f63098934117cd7a4d8175769923d16adcc
esrx <74571388+esrx@users.noreply.github.com>
2021-06-29 05:20:20 +0900

    Update script.js

--- script.js
+++ script.js
@@ -138,6 +138,8 @@ $('#output').text('ツイート：' + target);
 var target = target.replace(/%/g,'%25');
 var target = target.replace(/#/g,'%23');
 var target = target.replace(/&/g,'%26');
+var target = target.replace(/\s/g,'+');
+var target = target.replace(/++/g,'+');
 var target = target.replace(/\n/g,'%0A');
 $('#output').attr('href', 'https://twitter.com/intent/tweet?text=' + target);
 return false;
