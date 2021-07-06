5d831809b09cf97f857262138574c625801e0b60
esrx <74571388+esrx@users.noreply.github.com>
2021-07-06 17:35:59 +0900

    Update script.js

--- twitter/script.js
+++ twitter/script.js
@@ -1,1 +1,12 @@
+var AT = ""
+var AS = ""
 
+$('#tweet').click( sample );
+function sample() {
+var AT = $('#AT').val();
+var AS = $('#AS').val();
+var tweets = $('#texta').val();
+$('#output').text('ツイートしますか？：' + tweets);
+$('#output').attr('href', 'https://tweepypost.herokuapp.com/twitter?oauth_token=' + AT + '&oauth_verifier=' + AS + '&tweets=' + tweets);
+return false;
+}
