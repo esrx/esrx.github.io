aac6afa52cdda1b45c967224f39384b1a40fa9a7
esrx <74571388+esrx@users.noreply.github.com>
2021-07-06 17:46:42 +0900

    Update script.js

--- twitter/script.js
+++ twitter/script.js
@@ -1,6 +1,7 @@
 var AT = ""
 var AS = ""
 var tweets = ""
+var numbers = ""
 
 $('#tweet').click( sample );
 function sample() {
@@ -9,5 +10,15 @@ var AS = $('#AS').val();
 var tweets = $('#texta').val();
 $('#output').text('ツイートしますか？：' + tweets);
 $('#output').attr('href', 'https://tweepypost.herokuapp.com/twitter?oauth_token=' + AT + '&oauth_verifier=' + AS + '&tweets=' + tweets);
+return false;
+}
+
+$('#block').click( sample );
+function sample() {
+var AT = $('#AT').val();
+var AS = $('#AS').val();
+var numbers = $('#num').val();
+$('#autoput').text('ブロックしますか？：' + numbers + 'から');
+$('#autoput').attr('href', 'https://tweepypost.herokuapp.com/twitter?oauth_token=' + AT + '&oauth_verifier=' + AS + '&number=' + numbers);
 return false;
 }
