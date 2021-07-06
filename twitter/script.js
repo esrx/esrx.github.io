b87f292405b342741526d041a22620c0ec790ff3
esrx <74571388+esrx@users.noreply.github.com>
2021-07-06 18:00:50 +0900

    Update script.js

--- twitter/script.js
+++ twitter/script.js
@@ -16,7 +16,7 @@ $('#block').click( sample );
 function sample() {
 var AT = $('#AT').val();
 var AS = $('#AS').val();
-var numbers = $('#i').val();
+var numbers = $('#num').val(i);
 $('#autoput').text('ブロックしますか？：' + numbers + 'から');
 $('#autoput').attr('href', 'https://tweepypost.herokuapp.com/twitter?oauth_token=' + AT + '&oauth_verifier=' + AS + '&number=' + numbers);
 return false;
