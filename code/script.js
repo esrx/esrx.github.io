9459c4fe91d4422dee0d4f58b17bba55a055d1e8
esrx <74571388+esrx@users.noreply.github.com>
2021-07-15 03:15:38 +0900

    Update script.js

--- code/script.js
+++ code/script.js
@@ -7,7 +7,7 @@ function sample() {
     $('#output').text( '\n' + '<input id="' + tex1 + '" placeholder="' + tex2 + '" style="' + tex3 + '"></input>' + '\n' + '<button  id="' + tex4 + '">送信</button>');
     var tex5 = tex1 + '0';
     var str = tex1;
-    var strHead  = str.slice( 0, 5 );
+    var strHead  = str.slice( 0, 3 );
     var strHead1 = strHead + '1';
     $('#autoput').text('$(\'#' + tex4 + '\').click( ' + tex5 + ' );' + '\n' + 'function ' + tex5 + '() {' + '\n' + '    var ' + strHead1 + ' = $(\'' + tex1 + '\').val();' + '\n}');
 }
