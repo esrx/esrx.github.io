cba30a9516f5896b250b7bc584c5652a31d951f5
esrx <74571388+esrx@users.noreply.github.com>
2021-07-14 16:38:36 +0900

    Update script.js

--- code/script.js
+++ code/script.js
@@ -1,7 +1,11 @@
-var target = "";
+var tex1 = "";
+var tex2 = "";
+var tex3 = "";
 
 $('#inputid').click( sample );
 function sample() {
-    var target = $('#texta').val();
-    $('#output').html( 'あ' + '\n' + target );
+    var tex1 = $('#texta1').val();
+    var tex2 = $('#texta2').val();
+    var tex3 = $('#texta3').val();
+    $('#output').text( '<input id="' + tex1 + '" placeholder="' + tex2 + '" style="' + tex3 + '"></input>' );
 }
