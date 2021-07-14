44944a4530cc4917f08da54656cbec839eae5a12
esrx <74571388+esrx@users.noreply.github.com>
2021-07-14 16:56:17 +0900

    Update script.js

--- code/script.js
+++ code/script.js
@@ -7,5 +7,5 @@ function sample() {
     var tex1 = $('#texta1').val();
     var tex2 = $('#texta2').val();
     var tex3 = $('#texta3').val();
-    $('#output').text( '\n' + '<input id="' + tex1 + '" placeholder="' + tex2 + '" style="' + tex3 + '"></input>' + '\n');
+    $('#output').text( '\n' + '<input id="' + tex1 + '" placeholder="' + tex2 + '" style="' + tex3 + '"></input>' + '\n\n\n');
 }
