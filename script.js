3351f8c6eeab29dfe9d53b884d0f722bcdbfef57
esrx <74571388+esrx@users.noreply.github.com>
2021-07-16 03:38:19 +0900

    Update script.js

--- script.js
+++ script.js
@@ -26,6 +26,7 @@ var textclear = document.getElementById("texta");
 textclear.value = '';
 $('#output').text('');
 $('#input_copyoutput').text('');
+navigator.clipboard.writeText("")
 }
 
 $.fn.clickToggle = function (a, b) {
