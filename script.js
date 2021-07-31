f95dae244a611235c71c14463c66f3b846287480
esrx <74571388+esrx@users.noreply.github.com>
2021-08-01 08:22:31 +0900

    Update script.js

--- script.js
+++ script.js
@@ -33,8 +33,8 @@ $.fn.clickToggle = function (a,b) {
   return this.each(function () {
     var clicked = false;
     $(this).on('click',function () {
-      clicked = !clickd;
-      if(clickd) {
+      clicked = !clicked;
+      if(clicked) {
         return a.apply(this,arguments);
       }
     return b.apply(this,arguments);
