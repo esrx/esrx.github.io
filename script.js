ae96386625e580f44426fd2dc7c9f9daf84bd439
esrx <74571388+esrx@users.noreply.github.com>
2021-08-01 08:16:39 +0900

    Update script.js

--- script.js
+++ script.js
@@ -30,16 +30,16 @@ navigator.clipboard.writeText("");
 }
 
 $.fn.clickToggle = function (a, b) {
-return this.each(function () {
-var clicked = false;
-$(this).on('click', function () {
-clicked = !clicked;
-if (clicked) {
-return a.apply(this, arguments);
-}
-return b.apply(this, arguments);
-});
-});
+  return this.each(function () {
+    var clicked = false;
+    $(this).on('click', function () {
+      clicked = !clicked;
+      if(clicked) {
+        return a.apply(this, arguments);
+      }
+    return b.apply(this, arguments);
+    });
+  });
 };
 
 
