d8535abd104a8a841687732d92803bd8b42bff27
esrx <74571388+esrx@users.noreply.github.com>
2021-08-02 04:05:24 +0900

    Update script.js

--- google/script.js
+++ google/script.js
@@ -1,35 +1,31 @@
 document.addEventListener("DOMFocusIn", function(event) {
-
-var anc = location.hash
-if (anc == "#gsc.tab=1"){
-$('#offd').show();
-  }
-else if (anc == "#gsc.tab=0"){
-$('#offd').show();
-  }
-else if (anc == "#"){
-$('#offd').show();
-  }
-else{
-$('#offd').hide();
-  }
-
+   var anc = location.hash
+   if (anc == "#gsc.tab=1"){
+      $('#offd').show();
+   }
+   else if (anc == "#gsc.tab=0"){
+      $('#offd').show();
+   }
+   else if (anc == "#"){
+      $('#offd').show();
+   }
+   else{
+      $('#offd').hide();
+   }
 }, false);
 
 document.addEventListener("DOMFocusOut", function(event) {
-
-var anc = location.hash
-if (anc == "#gsc.tab=1"){
-$('#offd').show();
-  }
-else if (anc == "#gsc.tab=0"){
-$('#offd').show();
-  }
-else if (anc == "#"){
-$('#offd').show();
-  }
-else{
-$('#offd').hide();
-  }
-
+   var anc = location.hash
+   if (anc == "#gsc.tab=1"){
+      $('#offd').show();
+   }
+   else if (anc == "#gsc.tab=0"){
+      $('#offd').show();
+   }
+   else if (anc == "#"){
+      $('#offd').show();
+   }
+   else{
+      $('#offd').hide();
+   }
 }, false);
