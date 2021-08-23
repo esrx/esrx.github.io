0158dffa8101222f5366cd71f4d0359684e0a4cc
esrx <74571388+esrx@users.noreply.github.com>
2021-08-23 12:52:58 +0900

    Update script.js

--- script.js
+++ script.js
@@ -233,6 +233,6 @@ var target = target.replace('%','%25');
 var target = target.replace('+','%20');
 var target = target.replace('#','%23');
 var target = target.replace('&','%26');
-$('#output').attr('href', 'https://www.google.com/search?q=' + target + '+(iOS+OR+Android)+App#fpstate=aig');
+$('#output').attr('href', 'https://www.google.com/search?q=' + target + 'iOS+App#fpstate=aig');
 return false;
 }
