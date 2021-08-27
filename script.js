468ad49716d71af1298fe41ade2148643ed73c0b
esrx <74571388+esrx@users.noreply.github.com>
2021-08-28 04:51:53 +0900

    Update script.js

--- script.js
+++ script.js
@@ -176,7 +176,7 @@ $('#time').clickToggle(function (){
    $(this).next('.target');
    $("#time").html('📘until');
    var gets = document.getElementById("date").value;
-   var until = '&tbs=cdr%3A1%2Ccd_max%3A' + (gets.getMonth().toString()) + '%2F' + (gets.getDate().toString()) + '%2F' + (gets.getFullYear().toString());
+   var until = gets.getMonth().toString();
 }, function (){
    $(this).next('.target');
    $("#time").html('📕since');
