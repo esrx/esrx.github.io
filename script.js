01f3e57dfd7c2dfa701ec9f2ce17e49d3452a6d7
esrx <74571388+esrx@users.noreply.github.com>
2021-09-06 12:11:25 +0900

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ prev()、nextAll(),siblings()
 
 $('#mode').on('click',function() {
 $(this).siblings('#light,#dark').toggle();
+$('.twitter-timeline').each(function(){
+$(this).attr('data-theme','dark');
+});
 });
 
 function hidaris() {
