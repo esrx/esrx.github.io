5af392abc358eeff1493e4a37953d113bdc3ea0a
esrx <74571388+esrx@users.noreply.github.com>
2021-09-19 05:00:27 +0900

    Update script.js

--- script.js
+++ script.js
@@ -283,8 +283,6 @@ var target = target.replace('+','%20');
 var target = target.replace('#','%23');
 var target = target.replace('&','%26');
 $('#output').attr('href', 'https://open.spotify.com/search/song/' + target);
-window.open('spotify://search/track:' + target);
-setTimeout("window.open('https://open.spotify.com/search/song/' + target)",1000);
 return false;
 }
 
