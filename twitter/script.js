dd54202bb921b4a4a10f559b12dc5ef837d8d8e2
esrx <74571388+esrx@users.noreply.github.com>
2021-09-24 22:22:39 +0900

    Update script.js

--- twitter/script.js
+++ twitter/script.js
@@ -76,7 +76,7 @@ return false;
 };
 
 document.getElementById('explore').onclick = function() {
-document.getElementById('homeg').setAttribute("style", "fill:#dadada;");
+document.getElementById('homeg').setAttribute("style", "fill:#9da3ab;");
 document.getElementById('homes').setAttribute("d", homes);
 document.getElementById('homesd').setAttribute("d", homesd);
 document.getElementById('explores').setAttribute("d", "M22.06 19.94l-3.73-3.73C19.38 14.737 20 12.942 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c1.943 0 3.738-.622 5.21-1.67l3.73 3.73c.292.294.676.44 1.06.44s.768-.146 1.06-.44c.586-.585.586-1.535 0-2.12zM11 17c-3.308 0-6-2.692-6-6s2.692-6 6-6 6 2.692 6 6-2.692 6-6 6z");
@@ -90,7 +90,7 @@ return false;
 };
 
 document.getElementById('notification').onclick = function() {
-document.getElementById('homeg').setAttribute("style", "fill:#dadada;");
+document.getElementById('homeg').setAttribute("style", "fill:#9da3ab;");
 document.getElementById('homes').setAttribute("d", homes);
 document.getElementById('homesd').setAttribute("d", homesd);
 document.getElementById('explores').setAttribute("d", explores);
@@ -104,7 +104,7 @@ return false;
 };
 
 document.getElementById('message').onclick = function() {
-document.getElementById('homeg').setAttribute("style", "fill:#dadada;");
+document.getElementById('homeg').setAttribute("style", "fill:#9da3ab;");
 document.getElementById('homes').setAttribute("d", homes);
 document.getElementById('homesd').setAttribute("d", homesd);
 document.getElementById('explores').setAttribute("d", explores);
