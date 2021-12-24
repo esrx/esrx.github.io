48cb9e59ae32e5459a45e12f0746c689474438a8
esrx <74571388+esrx@users.noreply.github.com>
2021-12-24 17:36:17 +0900

    Update script.js

--- library/script.js
+++ library/script.js
@@ -2,6 +2,8 @@ var params = new URLSearchParams(window.location.search);
 var link = params.get("link");
 var links = decodeURIComponent(link);
 
+alert(links);
+
 regex = new RegExp('\\[[0-9].*\\]');
 var matchs = links.match(regex);
 var matches = matchs.match(new RegExp('[0-9].*'));
