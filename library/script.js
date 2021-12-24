c0f1ddeac9576de316576734b8d2bc2c61466b67
esrx <74571388+esrx@users.noreply.github.com>
2021-12-24 15:18:17 +0900

    Update script.js

--- library/script.js
+++ library/script.js
@@ -3,4 +3,10 @@ var link = params.get("link");
 
 var links = decodeURIComponent(link);
 
+regex = new RegExp('\\[[0-9].*\\]');
+
+if(regex.test(links)){
+
+}
+
 document.getElementById().src = links
